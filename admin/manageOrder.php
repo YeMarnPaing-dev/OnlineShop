75fbba3badb5baddda59737c09f0b2de180f97d5
Saw Ye Marn <yemarnpaing44@gmail.com>
2025-10-22 13:27:53 +0630

    OrderBackend

--- admin/manageOrder.php
+++ admin/manageOrder.php
@@ -1,3 +1,106 @@
 <?php include('widget/header.php') ?>
 
+<div class="container">
+    <div class="row pt-4 pb-4 gt3">
+        <?php
+        if(isset($_SESSION['noti'])){
+            echo $_SESSION['noti'];
+            unset($_SESSION['noti']);
+        }
+        ?>
+        
+      
+        <h2 class="mt-4 mb-3">Manage Order</h2>
+        <!-- table Categoru -->
+         <div class="col-12">
+            <!-- <a href="addProduct.php" class="btn btn-primary w-10 mb-3">Add Product</a> -->
+
+            <table class="table table-loght table-striped">
+  <thead>
+          <tr class="text-center">
+      <th scope="col">#</th>
+      <th scope="col">Product</th>
+      <th scope="col">Price</th>
+      <th scope="col">Qty</th>
+      <th scope="col">Total Price</th>
+      <th scope="col">Order Date</th>
+      <th scope="col">Status</th>
+      <th scope="col">Customer Name</th>
+      <th scope="col">Contact</th>
+      <th scope="col">Email</th>
+      <th scope="col">Address</th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php
+
+$sql = "SELECT *  FROM orders ";
+
+$res = mysqli_query($conn, $sql);
+
+// Check if query ran successfully
+if (!$res) {
+    die("Query Failed: " . mysqli_error($conn));
+}
+
+$count = mysqli_num_rows($res);
+
+if ($count == 0) {
+    echo '<tr><td colspan="8" class="text-center">No Data</td></tr>';
+} else {
+    $sn = 1;
+    while ($row = mysqli_fetch_assoc($res)) {
+        $id = $row['id'];
+        $product=$row['product'];
+        $price=$row['price'];
+        $qty=$row['qty'];
+        $total=$row['total'];
+        $date=$row['order_date'];
+        $status=$row['status'];
+        $name=$row['name'];
+        $contact=$row['contact'];
+        $email=$row['email'];
+        $address=$row['address'];
+        ?>
+
+        <tr class="text-center">
+            <th scope="row"><?= $sn ?></th>
+            <td><?= htmlspecialchars($product) ?></td>
+            <td><?= htmlspecialchars($price) ?></td>
+            <td><?= htmlspecialchars($qty) ?></td>
+            <td><?= htmlspecialchars($total) ?></td>
+            <td><?= htmlspecialchars($date) ?></td>
+            <td><?= htmlspecialchars($status) ?></td>
+            <td><?= htmlspecialchars($name) ?></td>
+            <td><?= htmlspecialchars($contact) ?></td>
+             <td><?= htmlspecialchars($email) ?></td>
+            <td><?= htmlspecialchars($address) ?></td>
+            
+            <td>
+                <a href="updateProduct.php?id=<?= urlencode($id) ?>" 
+                   class="bg-white p-2 rounded mx-1" title="Update Product">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </a>
+
+                <a href="deleteProduct.php?id=<?= urlencode($id) ?>&image=<?= urlencode($image_name) ?>" 
+                   class="bg-white p-2 rounded mx-1" title="Delete Product">
+                    <i class="fa-solid fa-trash text-danger"></i>
+                </a>
+            </td>
+        </tr>
+
+        <?php
+        $sn++;
+    }
+}
+?>
+
+   
+  </tbody>
+</table>
+         </div>
+    </div>
+</div>
+
 <?php include('widget/footer.php') ?>
